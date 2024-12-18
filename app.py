84b452719ca45517bb57e80ca63d4f550772b95f
austindodig <dodigaj@gmail.com>
2024-12-18 16:44:27 -0500

    socket update

--- app.py
+++ app.py
@@ -308,4 +308,5 @@ def handle_target_selected(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+
